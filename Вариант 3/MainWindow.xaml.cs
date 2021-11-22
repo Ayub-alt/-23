822d585d4a06d6dccf614cf13d856326d2711883
Ayub-alt <75316842+Ayub-alt@users.noreply.github.com>
2021-11-22 11:40:13 +0300

    Add files via upload

---
+++ Вариант 3/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Вариант_3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void but_MouseEnter(object sender, MouseEventArgs e)
+        {
+            but.Background = Brushes.LightGreen;
+        }
+
+        private void but_MouseLeave(object sender, MouseEventArgs e)
+        {
+            but.Background = Brushes.Red;
+
+        }
+
+        private void but_Click(object sender, RoutedEventArgs e)
+        {
+            txt.Text = null;
+            txt.Focus();
+            
+        }
+    }
+}
